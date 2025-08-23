865fd913fec235bdc08792e6f638e4858b7ca8f8
ABD-01 <63636498+ABD-01@users.noreply.github.com>
2025-08-23 22:51:25 +0530

    [temporary fix] moved `static void uart_init(void)` to separate file to prevent hardfault when accessing `RCC_APB2ENR`

--- main.c
+++ main.c
@@ -5,15 +5,8 @@
 #include "app.h"
 #include "reg.h"
 
-#define USART_FLAG_TXE  ((uint16_t)1 << 7) // Ref: rm0041 manual page637
-#define USART_FLAG_RXNE ((uint16_t)1 << 5) // Ref: rm0041 manual page637
-// See https://popovicu.com/posts/bare-metal-printf/ for UART driver
-// on baremetal platform
 
-#define USARTx     USART1
-#define USARTx_SR  USART1_SR
-#define USARTx_DR  USART1_DR
-#define USARTx_CR1 USART1_CR1
+
 
 static void uart_init(void);
 
@@ -99,17 +92,4 @@ skip_hf:
         }
     }
     return 0;
-}
-
-static void uart_init(void)
-{
-    *(RCC_APB2ENR) |= (uint32_t)(0x00000001 | 0x00000004);
-    *(RCC_APB1ENR) |= (uint32_t)(0x00020000);
-
-    /* USARTx Configuration */
-    *(GPIOA_CRL) = 0x00004B00;
-    *(GPIOA_CRH) = 0x44444444;
-
-    *(USARTx_CR1) = 0x0000000C;
-    *(USARTx_CR1) |= 0x2000;
 }
